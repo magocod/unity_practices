a6e00baf5ea358239822f3e70f3d8e85821dcc8b
magocod <magoyeison@gmail.com>
2025-06-30 18:15:07 -0400

    practice v1

--- roll_a_ball/Assets/Scripts/PlayerController.cs
+++ roll_a_ball/Assets/Scripts/PlayerController.cs
@@ -4,43 +4,92 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using TMPro;
+
+
 public class PlayerController : MonoBehaviour
 {
- // Rigidbody of the player.
- private Rigidbody rb; 
+    // Rigidbody of the player.
+    private Rigidbody rb;
 
- // Movement along X and Y axes.
- private float movementX;
- private float movementY;
+    // Movement along X and Y axes.
+    private float movementX;
+    private float movementY;
 
- // Speed at which the player moves.
- public float speed = 0; 
+    // Speed at which the player moves.
+    public float speed = 0;
 
- // Start is called before the first frame update.
- void Start()
+    private int count;
+
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
+
+    // Start is called before the first frame update.
+    void Start()
     {
- // Get and store the Rigidbody component attached to the player.
+        // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+
+        winTextObject.SetActive(false);
+
     }
- 
- // This function is called when a move input is detected.
- void OnMove(InputValue movementValue)
+
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
- // Convert the input value into a Vector2 for movement.
+        // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
 
- // Store the X and Y components of the movement.
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
- // FixedUpdate is called once per fixed frame-rate frame.
- private void FixedUpdate() 
+    void SetCountText()
     {
- // Create a 3D movement vector using the X and Y inputs.
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        countText.text = "Count: " + count.ToString();
 
- // Apply force to the Rigidbody to move the player.
-        rb.AddForce(movement * speed); 
+        if (count >= 4)
+        {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the current object
+            Destroy(gameObject); 
+            // Update the winText to display "You Lose!"
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
     }
 }
