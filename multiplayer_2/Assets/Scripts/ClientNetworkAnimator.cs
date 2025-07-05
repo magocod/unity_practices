2217fca095ad0dbb412f0ea39dd1a61962fbc7d2
magocod <magoyeison@gmail.com>
2025-07-04 21:40:00 -0400

    multiplayer 2

---
+++ multiplayer_2/Assets/Scripts/ClientNetworkAnimator.cs
@@ -0,0 +1,14 @@
+using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace NetcodeDemo
+{
+    [DisallowMultipleComponent]
+    public class ClientNetworkAnimator : NetworkAnimator
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
---
+++ multiplayer_2/Assets/Scripts/ServerPlayerSpawnPoints.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using DesignPatterns.Singleton;
+using System.Collections.Generic;
+
+public class ServerPlayerSpawnPoints : Singleton<ServerPlayerSpawnPoints>
+{
+    [SerializeField]
+    private List<GameObject> m_SpawnPoints;
+    public GameObject GetRandomSpawnPoint()
+    {
+        if (m_SpawnPoints.Count == 0)
+            return null;
+        return m_SpawnPoints[Random.Range(0, m_SpawnPoints.Count)];
+    }
+}
